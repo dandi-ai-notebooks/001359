23b774a4a0ef02910f2d9db6d27394748d4338c6
Jeremy Magland <jmagland@flatironinstitute.org>
2025-05-09 15:07:34 -0400

    updates

---
+++ v4/0.250401.1603/gemini-2.5-flash-preview-prompt-g-2/working/explore/plot_voltage_clamp.py
@@ -0,0 +1,35 @@
+# This script loads a VoltageClampSeries from an NWB file and plots a subset of the data.
+
+import pynwb
+import h5py
+import remfile
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load
+url = "https://api.dandiarchive.org/api/assets/c269347a-2d4b-4b6a-8b7c-2ef303ff503d/download/"
+remote_file = remfile.File(url)
+h5_file = h5py.File(remote_file)
+io = pynwb.NWBHDF5IO(file=h5_file)
+nwb = io.read()
+
+# Access VoltageClampSeries data
+# We will use data_00000_AD0
+data_series = nwb.acquisition["data_00000_AD0"]
+
+# Get a subset of the data and times
+num_points_to_plot = 10000
+data_subset = data_series.data[0:num_points_to_plot]
+starting_time = data_series.starting_time
+rate = data_series.rate
+time = starting_time + np.arange(num_points_to_plot) / rate
+
+# Plot the data
+plt.figure(figsize=(10, 6))
+plt.plot(time, data_subset)
+plt.xlabel("Time (s)")
+plt.ylabel(f"Current ({data_series.unit})")
+plt.title(f"Subset of {data_series.name} (Voltage Clamp Series)")
+plt.grid(True)
+plt.savefig("explore/voltage_clamp_plot.png")
+plt.close()
